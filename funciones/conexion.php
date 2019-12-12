78c5e4aa161afa214610c28841aa09ebc6b3c0d1
Juan Carlos mamani huayta <carloshuayta112@gmail.com>
2019-12-12 15:38:33 -0400

    iniciando

---
+++ funciones/conexion.php
@@ -0,0 +1,9 @@
+<?php 
+$conexion = mysqli_connect("localhost", "id11921574_root", "123123", "id11921574_laboratorio");
+if (!$conexion) {
+echo "Error al conectar base de datos";
+} else {
+	echo "";
+}
+
+?>
